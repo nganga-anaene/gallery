cf0b3f18ed3979b7af2b084c291feed792c538dd
Nganga <ngangaanaene@gmail.com>
2024-11-21 17:48:43 +0000

    database added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/wVqprXOZ4DSdknmFTDfsk6vpQTSPaZI1DzWRrAiV80eoOlcE",
@@ -10,12 +11,17 @@ const mockUrls = [
 const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }))
 
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany()
+  console.log(posts)
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + '-' + index} className="w-48">
             <img src={image.url} alt="image" />
           </div>
         ))}
